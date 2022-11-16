d3e7652f22c1734e27749b074035fc997d6b0c5f
J.R Perez <jrbaprz@gmail.com>
2022-11-16 11:39:48 -0500

    Added Weather API

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './index.css'
+
+const api = {
+  base: "http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=de209fe5c7c43d7c1f838eefcd2d0744"
+}
+
+const App = () => {
+  return (
+    <>
+    
+    </>
+  )
+}
+
+export default App
